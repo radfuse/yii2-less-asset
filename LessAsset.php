f1291720ef36215505fc1579bb220563a2d1af7f
Radfuse <info@radfuse.com>
2017-12-31 17:39:22 +0100

    Improvements and README

--- LessAsset.php
+++ LessAsset.php
@@ -15,6 +15,8 @@ class LessAsset extends AssetBundle
 
     ];
 
+    private $_less = false;
+
     public function publish($am){
         parent::publish($am);
         $this->publishLess($am);
@@ -25,12 +27,22 @@ class LessAsset extends AssetBundle
         
         if(!empty($this->less)){
             foreach($this->less as $lessFile){
-                if(is_file("$src/" . $this->sourceFolder . "/" . $lessFile . '.less')){
-                    $less = new \lessc();
-                    $less->setFormatter(YII_DEBUG ? "lessjs" : "compressed");
-                    if(is_file($this->basePath . "/" . $this->destinationFolder . "/" . $lessFile . ".css"))
-                        unlink($this->basePath . "/" . $this->destinationFolder . "/" . $lessFile . ".css");
-                    $less->compileFile("$src/" . $this->sourceFolder . "/" . $lessFile . ".less", $this->basePath . "/" . $this->destinationFolder . "/" . $lessFile . ".css");
+                $srcFile = "$src/" . $this->sourceFolder . "/" . $lessFile . '.less';
+                $destCss = $this->destinationFolder . "/" . $lessFile . ".css";
+                $destFile = $this->basePath . "/" . $destCss;
+
+                if(is_file($srcFile)){
+                    if(!$this->_less){
+                        $this->_less = new \lessc();
+                        $this->_less->setFormatter(YII_DEBUG ? "lessjs" : "compressed");
+                    }
+
+                    if(is_file($destFile))
+                        unlink($destFile);
+                    $this->_less->compileFile($srcFile, $destFile);
+
+                    if(is_file($destFile))
+                        $this->css[] = $destCss;
                 }
             }
         }
--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # yii2-less-asset
 Yii2 AssetBundle with lessc
+
+Installation
+============
+
+Installation with **composer**.
+
+> This instructions assumes that you have **composer** installed.
+
+Download using composer
+-------------------------------
+
+Add **yii2-less-asset** to the require section of your **composer.json** file:
+
+```js
+{
+    "require": {
+        "radfuse/yii2-less-asset": "dev-master"
+    }
+}
+```
+
+And run following command to download extension using **composer**:
+
+```bash
+$ php composer.phar update
+```
+
+OR
+
+```bash
+$ php composer.phar require radfuse/yii2-less-asset "dev-master"
+```
+
+Usage:
+--------
+
+Simply extend your asset from LessAsset, then add your less files list to the $less variable.
+
+
+```php
+...
+use radfuse\less\LessAsset;
+...
+
+class AppAsset extends LessAsset
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+
+    public $less = [
+        'main'
+    ];
+}
+...
+```
+
+Configuration
+=============
+
+LessAsset extends the base AssetBundle, so you can define the basic parameters (like $basePath). Also you can override some less-specific params:
+
+- sourceFolder:
+	- path of the less files under $basePath
+	- default: `less`
+- destinationFolder:
+	- path of the destination css files under $basePath
+	- default: `css`
+
+The default values for these params will read the @webroot/less/\*.less files and convert them to @webroot/css/\*.css files.
